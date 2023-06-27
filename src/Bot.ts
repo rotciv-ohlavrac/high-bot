79a1af26ece2f8b9bfc72deac7c521b2671372a7
= <=>
2023-06-27 18:53:58 -0300

    feat: :sparkles: load env variables

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,6 +1,10 @@
+import * as dotenv from "dotenv";
 import { Client, ClientOptions } from "discord.js";
 import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
+dotenv.config();
+
+console.log("variables =>", process.env.DISCORD_TOKEN);
 
 const token =
   "MTEyMzM2MDQ5MzQ5NTY0ODM1Nw.G-CBHz.9Catd3pptpsNfotXHuSFnf5Rubp-dj224ZNMv0"; // add your token here
