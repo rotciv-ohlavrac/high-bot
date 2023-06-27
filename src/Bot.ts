3aff4fd1b7aa511d0ac6601fe152bb65c46d27f9
= <=>
2023-06-27 19:00:23 -0300

    feat: :sparkles: get discord token from the .env file

--- src/Bot.ts
+++ src/Bot.ts
@@ -4,10 +4,7 @@ import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
 dotenv.config();
 
-console.log("variables =>", process.env.DISCORD_TOKEN);
-
-const token =
-  "MTEyMzM2MDQ5MzQ5NTY0ODM1Nw.G-CBHz.9Catd3pptpsNfotXHuSFnf5Rubp-dj224ZNMv0"; // add your token here
+const token = process.env.DISCORD_TOKEN; // add your token here
 
 console.log("Bot is starting...");
 
